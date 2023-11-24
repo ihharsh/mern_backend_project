94f53c15b047ddac6792738db95124ef08724f23
ihharsh <116948587+ihharsh@users.noreply.github.com>
2023-11-24 13:06:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ dotenv.config();
 
 app.use(express.json());
 
-const userRoutes = require("./Routes/userRoutes");
-const chatRoutes = require("./Routes/chatRoutes");
-const messageRoutes = require("./Routes/messageRoutes");
+const userRoutes = require("/Routes/userRoutes");
+const chatRoutes = require("/Routes/chatRoutes");
+const messageRoutes = require("/Routes/messageRoutes");
 
 const connectDb = async () => {
   try {
